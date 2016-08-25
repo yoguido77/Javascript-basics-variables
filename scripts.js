404171901e0345914d674b7a030eb82ef00d472c
yoguido77 <yoguido77@gmail.com>
2016-08-25 15:14:49 -0500

    Working with javascript variables

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var message = "Hello!"
+alert(message);
+message = "Welcome to Javascript Basics!"
+alert(message);
+message = "She's a great person";
+var htmlSnippet = '<h1 class="special">Important Headline</h1>';
+
+// Using escape character
+
+message = 'She\'s a great person!';
+htmlSnippet = "<h1 class=\"special\">Important Headline</h1>";
